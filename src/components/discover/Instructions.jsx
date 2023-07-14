407e2fc6c4e6dcc88220a4562a1cf45ca987cce5
nikhilkxmar <nik37979@gmail.com>
2023-07-14 22:15:44 +0530

    sixth commit

--- src/components/discover/Instructions.jsx
+++ src/components/discover/Instructions.jsx
@@ -13,8 +13,8 @@ const Instructions = () => {
                     <div className='w-full flex justify-center'>
                       <LuPackageSearch className='w-[40px] h-[40px] pointer-events-none'/>
                     </div>
-                    <h3 className='font-medium font-playfair text-[22px] my-6 text-center'>Search</h3>
-                      <p className='text-gray-600 font-playfair text-[19px] text-center'>Discover audiobooks from your favorite authors on our decentralized platform. Use the search function to find authors by address and explore their collection of captivating audiobooks. Whether you're a fan of mystery, romance, or science fiction, searching by author allows you to easily access their works and embark on exciting audio adventures.</p>
+                    <h3 className='font-medium font-playfair text-[22px] my-6 text-center'>{'Search'}</h3>
+                      <p className='text-gray-600 font-playfair text-[19px] text-center'>{'Discover audiobooks from your favorite authors on our decentralized platform. Use the search function to find authors by address and explore their collection of captivating audiobooks. Whether youre a fan of mystery, romance, or science fiction, searching by author allows you to easily access their works and embark on exciting audio adventures.'}</p>
                   </div>
               </div>
               <div className='bg-white rounded-[5px] shadow-sm border-[1.75px] border-black'>
@@ -22,8 +22,8 @@ const Instructions = () => {
                     <div className='w-full flex justify-center'>
                         <GiFizzingFlask className='w-[40px] h-[40px] pointer-events-none'/>
                     </div>
-                    <h3 className='font-medium font-playfair text-[22px] my-6 text-center'>Try</h3>
-                      <p className='text-gray-600 font-playfair text-[19px] text-center'>Curious about a particular audiobook? Dive into a sample to get a taste of the story before making a commitment. With the "Play Sample" feature, you can listen to a portion of the audio for free. Immerse yourself in the narrator's voice, the captivating plot, and the immersive world of the audiobook. It's a great way to determine if the audiobook is right for you.</p>
+                    <h3 className='font-medium font-playfair text-[22px] my-6 text-center'>{'Try'}</h3>
+                      <p className='text-gray-600 font-playfair text-[19px] text-center'>{'Curious about a particular audiobook? Dive into a sample to get a taste of the story before making a commitment. With the "Play Sample" feature, you can listen to a portion of the audio for free. Immerse yourself in the narrators voice, the captivating plot, and the immersive world of the audiobook. Its a great way to determine if the audiobook is right for you.'}</p>
                   </div>
               </div>
               <div className='bg-white rounded-[5px] shadow-sm border-[1.75px] border-black'>
@@ -31,8 +31,8 @@ const Instructions = () => {
                     <div className='w-full flex justify-center'>
                         <BiPurchaseTagAlt className='w-[40px] h-[40px] pointer-events-none'/>
                     </div>
-                    <h3 className='font-medium font-playfair text-[22px] my-6 text-center'>Purchase</h3>
-                      <p className='text-gray-600 font-playfair text-[19px] text-center'> Ready to embark on a full audiobook experience? Purchase your desired audiobook and enjoy hours of immersive storytelling. Our platform offers a convenient and secure way to buy audiobooks. Simply choose the audiobook you want to own, follow the purchase process, and gain access to a world of incredible narratives. It's time to enrich your audio library.</p>
+                    <h3 className='font-medium font-playfair text-[22px] my-6 text-center'>{'Purchase'}</h3>
+                      <p className='text-gray-600 font-playfair text-[19px] text-center'>{' Ready to embark on a full audiobook experience? Purchase your desired audiobook and enjoy hours of immersive storytelling. Our platform offers a convenient and secure way to buy audiobooks. Simply choose the audiobook you want to own, follow the purchase process, and gain access to a world of incredible narratives. Its time to enrich your audio library.'}</p>
                   </div>
               </div>
           </div>
--- src/components/home/Content.jsx
+++ src/components/home/Content.jsx
@@ -7,9 +7,9 @@ const Content = () => {
     <div className='mt-[75px] max-w-[1240px] mx-auto mb-[20px]'>
         <div className='flex justify-between items-center'>
             <div className='w-[49%]'>
-                <h2 className='text-[40px] font-playfair'>Listen to the top books</h2>
-                <p className='text-[20px] font-playfair mt-[20px] text-gray-700'>Immerse yourself in a world of literary excellence with our curated collection of top books available for listening. Indulge in the finest narratives crafted by celebrated authors, where every word comes to life through the power of audio. From gripping thrillers to heartwarming tales, thought-provoking non-fiction to captivating fiction, our selection represents the very best of literature across genres. Sit back, relax, and let the captivating voices of talented narrators transport you to extraordinary realms and ignite your imagination.</p>
-                <p className='text-[20px] font-playfair mt-[30px] text-gray-700 italic'>"Immerse yourself in the symphony of words as you listen to the top books, where stories come alive and ignite the spark of imagination within"</p>
+                <h2 className='text-[40px] font-playfair'>{'Listen to the top books'}</h2>
+                <p className='text-[20px] font-playfair mt-[20px] text-gray-700'>{'Immerse yourself in a world of literary excellence with our curated collection of top books available for listening. Indulge in the finest narratives crafted by celebrated authors, where every word comes to life through the power of audio. From gripping thrillers to heartwarming tales, thought-provoking non-fiction to captivating fiction, our selection represents the very best of literature across genres. Sit back, relax, and let the captivating voices of talented narrators transport you to extraordinary realms and ignite your imagination.'}</p>
+                <p className='text-[20px] font-playfair mt-[30px] text-gray-700 italic'>{'"Immerse yourself in the symphony of words as you listen to the top books, where stories come alive and ignite the spark of imagination within"'}</p>
             </div>
             <div className='w-[49%] flex justify-end'>
                 <Image src={books} alt='image' className='w-[450px] h-[450px] opacity-90 rounded-[7px]'/>
